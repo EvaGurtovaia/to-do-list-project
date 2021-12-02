eb7ae705fea07183b9315f0e2d85d5d6000d37de
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-12-02 15:48:13 -0600

    Refactoring the structure of components

--- to-do-list-project/src/ToDo.js
+++ to-do-list-project/src/ToDo.js
@@ -1,39 +1,15 @@
 import React, { Component } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 class ToDo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            todos: [
-                {
-                    task: "Organize Garage",
-                    id: uuidv4(),
-                    completed: false,
-                },
-                {
-                    task: "Bake Cookies",
-                    id: uuidv4(),
-                    completed: false,
-                },
-            ],
-        };
-    }
-
-    renderItems() {
-        return (
-            <ul>
-                {this.state.todos.map((todo) => (
-                    <li key={todo.id}>{todo.task}</li>
-                ))}
-            </ul>
-        );
-    }
     render() {
         return (
             <div>
                 <h2>To Do List</h2>
-                <div className="ToDo">{this.renderItems()}</div>
+                <li>
+                    <div className="ToDo">{this.props.task}</div>
+                </li>
+                <button>Edit</button>
+                <button>X</button>
             </div>
         );
     }
--- to-do-list-project/src/ToDoList.js
+++ to-do-list-project/src/ToDoList.js
@@ -1,14 +1,37 @@
 import React, { Component } from "react";
 import ToDo from "./ToDo";
 import NewToDoForm from "./NewToDoForm";
+import { v4 as uuidv4 } from "uuid";
 
 class ToDoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: [
+                {
+                    task: "Organize Garage",
+                    id: uuidv4(),
+                    completed: false,
+                },
+                {
+                    task: "Bake Cookies",
+                    id: uuidv4(),
+                    completed: false,
+                },
+            ],
+        };
+    }
+
     render() {
+        const todos = this.state.todos.map((todo) => {
+            return <ToDo key={todo.id} id={todo.id} task={todo.task} />;
+        });
+
         return (
             <div>
-                <div className="ToDoList">
-                    <ToDo />
-                </div>
+                <ul>
+                <div className="ToDoList">{todos}</div>
+                </ul>
                 <NewToDoForm />
             </div>
         );
