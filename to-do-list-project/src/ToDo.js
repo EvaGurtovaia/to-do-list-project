7425ae9bef2275417fd813994ea36e4f2ffbfdc1
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-12-05 13:29:02 -0600

    Working on editing functionality

--- to-do-list-project/src/ToDo.js
+++ to-do-list-project/src/ToDo.js
@@ -1,17 +1,36 @@
 import React, { Component } from "react";
 
 class ToDo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEditing: false,
+        };
+        this.addNewTodo = this.addNewTodo.bind(this);
+    }
     render() {
-        return (
-            <div>
-                <h2>To Do List</h2>
-                <li>
-                    <div className="ToDo">{this.props.task}</div>
-                </li>
-                <button>Edit</button>
-                <button onClick={this.props.delete}>X</button>
-            </div>
-        );
+        let result;
+        if (this.state.isEditing) {
+            result = (
+                <div>
+                    <form>
+                        <input type="text" />
+                    </form>
+                </div>
+            );
+        } else {
+            result = (
+                <div>
+                    <h2>To Do List</h2>
+                    <li>
+                        <div className="ToDo">{this.props.task}</div>
+                    </li>
+                    <button>Edit</button>
+                    <button onClick={this.props.delete}>X</button>
+                </div>
+            );
+        }
+        return result;
     }
 }
 export default ToDo;
